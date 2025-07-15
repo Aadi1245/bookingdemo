bbca00cdbc3cd76dc1f20b690addcad0f4a332f0
Aaditya Gupta <aadtiyagupta@itgeeks.com>
2025-07-15 18:37:36 +0530

    login screen update

--- lib/screens/login_with_otp_screen.dart
+++ lib/screens/login_with_otp_screen.dart
@@ -1,7 +1,10 @@
 import 'package:demoappfortest/utils/app_colors.dart';
 import 'package:demoappfortest/utils/app_custom_theme.dart';
 import 'package:demoappfortest/utils/app_sizes.dart';
+import 'package:demoappfortest/utils/app_typography.dart';
+import 'package:demoappfortest/utils/app_widget_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../utils/app_custom_theme.dart';
 
@@ -14,73 +17,50 @@ class LoginWithOtpScreen extends StatefulWidget {
 
 class _LoginWithOtpScreenState extends State<LoginWithOtpScreen>
     with TickerProviderStateMixin {
-  late final AnimationController _textController;
-  late final AnimationController _arrowController;
-  late final AnimationController _fieldController;
-  late final AnimationController _btnController;
+  late final AnimationController _containerController;
+  late final Animation<Offset> _containerOffset;
+  late final Animation<double> _containerOpacity;
 
-  late final Animation<Offset> _textOffset;
-  late final Animation<Offset> _arrowOffset;
-  late final Animation<Offset> _fieldOffset;
-  late final Animation<Offset> _btnOffset;
-
-  bool showText = false;
-  bool showArrow = false;
-  bool showField = false;
-  bool showBtn = false;
+  bool showContainer = false;
+  bool isValidNumber = false;
 
   @override
   void initState() {
     super.initState();
 
-    _textController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 700));
-    _arrowController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 700));
-    _fieldController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 700));
-    _btnController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 700));
+    _containerController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 800));
 
-    _textOffset = Tween<Offset>(begin: Offset(0, -2), end: Offset.zero).animate(
-      CurvedAnimation(parent: _textController, curve: Curves.bounceOut),
-    );
-    _arrowOffset =
-        Tween<Offset>(begin: Offset(0, -2), end: Offset.zero).animate(
-      CurvedAnimation(parent: _arrowController, curve: Curves.bounceOut),
-    );
-    _fieldOffset =
-        Tween<Offset>(begin: Offset(0, -2), end: Offset.zero).animate(
-      CurvedAnimation(parent: _fieldController, curve: Curves.bounceOut),
-    );
-    _btnOffset = Tween<Offset>(begin: Offset(0, -2), end: Offset.zero).animate(
-      CurvedAnimation(parent: _btnController, curve: Curves.bounceOut),
+    _containerOffset =
+        Tween<Offset>(begin: Offset(0, 2), end: Offset.zero).animate(
+      CurvedAnimation(parent: _containerController, curve: Curves.elasticOut),
     );
 
-    _startDropSequence();
+    _containerOpacity = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _containerController, curve: Curves.easeInOut),
+    );
+
+    _startAnimation();
   }
 
-  Future<void> _startDropSequence() async {
-    setState(() => showText = true);
-    await _textController.forward();
-
-    setState(() => showArrow = true);
-    await _arrowController.forward();
-
-    setState(() => showField = true);
-    await _fieldController.forward();
-
-    setState(() => showBtn = true);
-    await _btnController.forward();
+  Future<void> _startAnimation() async {
+    await Future.delayed(Duration(milliseconds: 300));
+    setState(() => showContainer = true);
+    await _containerController.forward();
   }
 
   @override
   void dispose() {
-    _textController.dispose();
-    _arrowController.dispose();
-    _fieldController.dispose();
-    _btnController.dispose();
+    _containerController.dispose();
     super.dispose();
+  }
+
+  TextEditingController _phoneController = TextEditingController();
+
+  void _validatePhoneNumber(String value) {
+    setState(() {
+      isValidNumber = value.length == 10 && RegExp(r'^[0-9]+$').hasMatch(value);
+    });
   }
 
   @override
@@ -101,68 +81,60 @@ class _LoginWithOtpScreenState extends State<LoginWithOtpScreen>
         child: Column(
           children: [
             Spacer(),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.6,
-              // margin: ,
-              padding: AppSizes.responsivePadding(context),
-              decoration: BoxDecoration(
-                color: AppColors.lightColorScheme.onPrimary,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(AppSizes.radiusXl),
-                    topRight: Radius.circular(AppSizes.radiusXl)),
-                boxShadow: [
-                  customTheme.shadows.small.first,
-                  // BoxShadow(
-                  //   color: Colors.black26,
-                  //   blurRadius: 8,
-                  //   offset: Offset(0, 4),
-                  // ),
-                ],
-              ),
-              child: Center(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // 1. TEXT
-                      if (showText)
-                        SlideTransition(
-                          position: _textOffset,
-                          child: Text(
-                            "Enter your phone number for verification",
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black,
-                              shadows: [
-                                Shadow(
-                                  offset: Offset(0, 1),
-                                  blurRadius: 4,
-                                  color: Colors.black26,
-                                )
-                              ],
+            if (showContainer)
+              SlideTransition(
+                position: _containerOffset,
+                child: FadeTransition(
+                  opacity: _containerOpacity,
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    padding: AppSizes.responsivePadding(context),
+                    decoration: BoxDecoration(
+                      color: AppColors.lightColorScheme.onPrimary,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(AppSizes.radiusXl),
+                          topRight: Radius.circular(AppSizes.radiusXl)),
+                      boxShadow: [
+                        customTheme.shadows.small.first,
+                      ],
+                    ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                              height: AppSizes.isMobile(context)
+                                  ? AppSizes.sm
+                                  : AppSizes.xl),
+                          // 1. TEXT
+                          Text(
+                            "Phone Verification",
+                            style:
+                                AppTypography.textTheme.titleMedium!.copyWith(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black87,
                             ),
                             textAlign: TextAlign.center,
                           ),
-                        ),
-                      if (showText) SizedBox(height: 30),
+                          SizedBox(
+                              height: AppSizes.isMobile(context)
+                                  ? AppSizes.md
+                                  : AppSizes.xl),
+                          Text(
+                            "We'll send you a verification code",
+                            style: AppTypography.textTheme.titleSmall!.copyWith(
+                                fontSize: 12, color: Colors.grey[600]),
+                            textAlign: TextAlign.center,
+                          ),
 
-                      // 2. ARROW
-                      // if (showArrow)
-                      //   SlideTransition(
-                      //     position: _arrowOffset,
-                      //     child: Image.asset(
-                      //       "assets/gif/arrow_down.gif",
-                      //       height: 70,
-                      //     ),
-                      //   ),
-                      // if (showArrow) SizedBox(height: 30),
+                          SizedBox(
+                              height: AppSizes.isMobile(context)
+                                  ? AppSizes.lg
+                                  : AppSizes.xxl),
 
-                      // 3. TEXT FIELD
-                      if (showField)
-                        SlideTransition(
-                          position: _fieldOffset,
-                          child: Container(
+                          // 3. TEXT FIELD
+                          Container(
+                            height: AppSizes.buttonHeight,
                             decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(14),
@@ -170,15 +142,62 @@ class _LoginWithOtpScreenState extends State<LoginWithOtpScreen>
                                 BoxShadow(
                                   blurRadius: 8,
                                   color: Colors.black26,
-                                  offset: Offset(0, 4),
+                                  offset: Offset(0, 1),
                                 ),
                               ],
                             ),
                             child: TextField(
+                              onChanged: _validatePhoneNumber,
+                              controller: _phoneController,
                               keyboardType: TextInputType.phone,
+                              maxLength: 10,
+                              inputFormatters: [
+                                FilteringTextInputFormatter.digitsOnly,
+                                LengthLimitingTextInputFormatter(10),
+                              ],
                               decoration: InputDecoration(
+                                prefixIcon: Container(
+                                  margin: EdgeInsets.only(
+                                    right: AppSizes.sm,
+                                  ),
+                                  width: 65,
+                                  alignment: Alignment.center,
+                                  child: Row(
+                                    children: [
+                                      Spacer(),
+                                      Icon(
+                                        Icons.phone_outlined,
+                                        color: AppColors.neutral500,
+                                        size: AppSizes.md + 2,
+                                      ),
+                                      Spacer(),
+                                      Text(
+                                        "+91",
+                                        style: AppTypography
+                                            .textTheme.labelLarge!
+                                            .copyWith(
+                                          fontSize: AppSizes.md,
+                                          color: AppColors.neutral800,
+                                        ),
+                                      ),
+                                      Spacer(),
+                                      Container(
+                                        width: 1,
+                                        height: AppSizes.lg + 21,
+                                        color: Colors.grey[600],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                suffixIcon: isValidNumber
+                                    ? Icon(
+                                        Icons.check_circle,
+                                        color: Colors.green,
+                                        size: AppSizes.md + 4,
+                                      )
+                                    : null,
                                 hintText: 'Mobile Number',
-                                hintStyle: TextStyle(color: Colors.grey[600]),
+                                hintStyle: AppTypography.textTheme.labelLarge,
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(14),
                                     borderSide: BorderSide.none),
@@ -186,44 +205,93 @@ class _LoginWithOtpScreenState extends State<LoginWithOtpScreen>
                                     vertical: 18, horizontal: 20),
                                 filled: true,
                                 fillColor: Colors.white,
+                                counterText: '', // Hide the counter text
                               ),
                             ),
                           ),
-                        ),
-                      if (showField) SizedBox(height: 24),
+                          SizedBox(height: 24),
 
-                      // 4. BUTTON
-                      if (showBtn)
-                        SlideTransition(
-                          position: _btnOffset,
-                          child: SizedBox(
+                          // 4. BUTTON
+                          SizedBox(
                             width: double.infinity,
+                            height: AppSizes.buttonHeight,
                             child: ElevatedButton(
                               style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.white,
-                                foregroundColor: const Color(0xFFD84E55),
-                                elevation: 6,
-                                padding: EdgeInsets.symmetric(vertical: 16),
+                                backgroundColor: isValidNumber
+                                    ? AppColors.primaryBlue
+                                    : AppColors.neutral300,
+                                elevation: 3,
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(14),
                                 ),
                               ),
-                              onPressed: () {
-                                // Handle register
-                              },
-                              child: Text(
-                                "Register",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              onPressed: isValidNumber
+                                  ? () {
+                                      // Handle register
+                                    }
+                                  : null,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Spacer(),
+                                  Icon(Icons.send,
+                                      color: isValidNumber
+                                          ? AppColors.neutral100
+                                          : AppColors.neutral400,
+                                      size: AppSizes.md + 2),
+                                  SizedBox(width: AppSizes.sm),
+                                  Text(
+                                    "Send verification code",
+                                    style: AppTypography.textTheme.labelLarge!
+                                        .copyWith(
+                                      fontSize: AppSizes.md,
+                                      color: isValidNumber
+                                          ? AppColors.neutral100
+                                          : AppColors.neutral400,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                ],
                               ),
                             ),
                           ),
-                        ),
-                    ],
+
+                          SizedBox(height: AppSizes.md),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have an account? ",
+                                style: AppTypography.textTheme.labelLarge!
+                                    .copyWith(
+                                  fontSize: AppSizes.md - 3,
+                                  color: AppColors.neutral400,
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  // Handle navigation to register screen
+                                },
+                                child: Text(
+                                  "Sign up",
+                                  style: AppTypography.textTheme.labelLarge!
+                                      .copyWith(
+                                    fontSize: AppSizes.md - 3,
+                                    color: AppColors.primaryBlue,
+                                    fontWeight: FontWeight.w600,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
           ],
         ),
       ),
